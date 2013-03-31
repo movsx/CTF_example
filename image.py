3acbed404277c7d69fcfcd418cb459586bf1a7ce
movsx <movsx@mail.ru>
2013-03-31 23:14:16 +0400

    Пример работы с техникой bits-per-pixel

--- image.py
+++ image.py
@@ -16,25 +16,43 @@ def diff_images(steg_pixels, orig_pixels, y, x):
 
 ### Блок для открытия файла, его загрузки и печати пикселей
 # Примеры работы с функциями графической библиотеки PIL
-im = Image.open('b.jpg', 'r') # Открыть изображение в режиме для чтени
-pixels = im.load()
-width, height = im.size # получить размер картинки в pixel
-
-# пройтись по каждому пикселю, просто определить цвета
-#for y in xrange(height): # Iterate through each pixel
-#    for x in xrange(width):
-#        print get_pixels(pixels, y,x)
+def load_image_file():
+    im = Image.open('b.jpg', 'r') # Открыть изображение в режиме для чтени
+    pixels = im.load()
+    width, height = im.size # получить размер картинки в pixel
+    # пройтись по каждому пикселю, просто определить цвета
+    for y in xrange(height): # Iterate through each pixel
+        for x in xrange(width):
+            print get_pixels(pixels, y,x)
 
 ### Блок для накладывания первой картинки на вторую
-im2 = Image.open('c.jpg', 'r') # Открыть изображение в режиме для чтени
-pixels2 = im2.load()
-width2, height2 = im2.size # получить размер картинки в pixel
-new=Image.new("RGB",(width,height))
-for y in xrange(height): # Iterate through each pixel
-    for x in xrange(width):
-        new.putpixel((x,y), diff_images(pixels, pixels2, y,x))
-new.save("end.png")
+def xor_image_file():
+    im = Image.open('b.jpg', 'r') # Открыть изображение в режиме для чтени
+    pixels = im.load()
+    width, height = im.size # получить размер картинки в pixel
+    im2 = Image.open('c.jpg', 'r') # Открыть изображение в режиме для чтени
+    pixels2 = im2.load()
+    width2, height2 = im2.size # получить размер картинки в pixel
+    new=Image.new("RGB",(width,height))
+    for y in xrange(height): # Iterate through each pixel
+        for x in xrange(width):
+            new.putpixel((x,y), diff_images(pixels, pixels2, y,x))
+    new.save("end.png")
 
+
+BPP = 4
+### Блок для примера работы с техникой bits-per-pixel
+def example_bpp():
+    with open('image_diff.png','rb') as f:
+        image = f.read()
+    bts = []
+    [bts.extend([b for b in '00000000'[len(bin(ord(c))[2:]):] + bin(ord(c))[2:]]) for c in image]
+    with open('out.raw', 'wb') as f:
+        for i in range(len(bts) // BPP):
+            chunk = ''.join(bts[i * BPP : (i + 1) * BPP])
+            f.write(chr(int(chunk, 2)))
+
+#example_bpp()
 
 
 """
